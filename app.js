d26d3a46531ba2156a4d25fe37a6a2cc4a41ab2e
perasarvani <sarvanipera123@gmail.com>
2023-03-29 10:56:43 -0500

    updated js files

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var mydatarouter = require('./routes/mydata');
 var usersRouter = require('./routes/users');
+var computeRouter = require('./routes/compute');
 
 var app = express();
 
@@ -21,6 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/mydata', mydatarouter);
+app.use('/compute', computerouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
