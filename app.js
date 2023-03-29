4289d7b496cc43ab7ea39a60c05e4d2b66375d32
perasarvani <sarvanipera123@gmail.com>
2023-03-29 12:01:01 -0500

    updated

--- app.js
+++ app.js
@@ -24,7 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/mydata', mydatarouter);
-app.use('/compute', computerouter);
+app.use('/compute', computeRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/compute.js
+++ routes/compute.js
@@ -8,17 +8,17 @@ router.get('/', function(req, res, next) {
     if(req.query.x == undefined){
         x=Math.floor(Math.random()*10)
         y=Math.floor(Math.random()*5);
-        let result = Math.atan(x);
-        let result2 = Math.exp(x);
-        let result3= Math.expm1(y);
-        res.send('Math.atan() applied for' +x +'is' +result+'\n Math.exp() applied for' +x +'is' +result2+'\n Math.expm1() applied for number' +y+'is'+result3);
+        let sarvani = Math.atan(x);
+        let sarvani2 = Math.exp(x);
+        let sarvani3= Math.expm1(y);
+        res.send('Math.atan() applied for' +x +'is' +sarvani+'\n Math.exp() applied for' +x +'is' +sarvani2+'\n Math.expm1() applied for number' +y+'is'+sarvani3);
     }
     else{
         x =req.query.x;
-        let result = Math.atan(x);
-        let result2 = Math.exp(x);
-        let result3= Math.expm1(y);
-        res.send('Math.atan() applied for' +x +'is' +result+'\n Math.exp() applied for' +x +'is' +result2+'\n Math.expm1() applied for number' +y+'is'+result3);
+        let sarvani = Math.atan(x);
+        let sarvani2 = Math.exp(x);
+        let sarvani3= Math.expm1(y);
+        res.send('Math.atan() applied for' +x +'is' +sarvani+'\n Math.exp() applied for' +x +'is' +sarvani2+'\n Math.expm1() applied for number' +y+'is'+sarvani3);
     }
 });
 
